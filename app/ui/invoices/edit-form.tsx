955d4000aaa707e95a02137aec3e99c2cac5e387
Mustafa Syahmi <mussyahmi31@gmail.com>
2024-02-14 14:37:19 +0800

    Chapter 15: Adding Authentication

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -145,6 +145,14 @@ export default function EditInvoiceForm({
               ))}
           </div>
         </fieldset>
+
+        <div aria-live="polite" aria-atomic="true">
+          {state.message && (
+            <p className="mt-2 text-sm text-red-500">
+              {state.message}
+            </p>
+          )}
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
